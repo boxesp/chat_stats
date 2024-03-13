e7dcaea99cef49e6416bbf114b138272533fb0fa
boxesp <41246312+boxesp@users.noreply.github.com>
2024-03-13 09:45:34 -0300

    Update chatStatistics.js

--- js/chatStatistics.js
+++ js/chatStatistics.js
@@ -1,7 +1,7 @@
 import { setSessionStartTime, calculateSessionDuration } from "./timer.js";
 
 const urlParams = new URLSearchParams(window.location.search);
-const streamerList = ["xqc", "ludwig", "adinross"]; // List of streamers
+const streamerList = ["xqc", "jesss", "adinross"]; // List of streamers
 const topListLength = urlParams.get("listLength") || 5;
 let currentStreamerIndex = 0;
 let reconnectWebSocket = true; // Flag to control WebSocket reconnection
