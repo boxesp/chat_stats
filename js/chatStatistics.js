6d8f7f7338dca5cf347a1a47fdbab4084823bfb2
boxesp <41246312+boxesp@users.noreply.github.com>
2024-03-13 09:48:52 -0300

    Update chatStatistics.js

--- js/chatStatistics.js
+++ js/chatStatistics.js
@@ -1,7 +1,7 @@
 import { setSessionStartTime, calculateSessionDuration } from "./timer.js";
 
 const urlParams = new URLSearchParams(window.location.search);
-const channel = urlParams.get("channel") || "jesss";
+const channel = urlParams.get("channel") || "god";
 const topListLength = urlParams.get("listLength") || 5;
 
 // channel name page element
@@ -62,6 +62,12 @@ function connectWebSocket() {
     setSessionStartTime(); // Set the session start time when the WebSocket connection opens
     updateIsLiveStatus();
     await fetchViewerCount();
+
+    const channelExists = await checkStreamerExists(channel);
+    if (!channelExists) {
+      // If the channel doesn't exist, insert it into the database
+      await insertStreamer(channel);
+    }
   });
 
   // WebSocket error event listener
@@ -75,6 +81,29 @@ function connectWebSocket() {
     // Attempt to reconnect after a delay
     setTimeout(connectWebSocket, 5000);
   });
+}
+
+async function checkStreamerExists(channel) {
+  try {
+    const response = await fetch(`http://127.0.0.1:3000/checkStreamer?channel=${channel}`);
+    const data = await response.json();
+    return data.channelExists;
+  } catch (error) {
+    console.error("Error checking if streamer exists:", error);
+    return false;
+  }
+}
+
+async function insertStreamer(channel) {
+  try {
+    const response = await fetch(`http://127.0.0.1:3000/insertStreamer?channel=${channel}`, {
+      method: 'GET'
+    });
+    const data = await response.json();
+    console.log(`Streamer '${channel}' inserted into the database.`);
+  } catch (error) {
+    console.error("Error inserting streamer:", error);
+  }
 }
 
 // handle the WebSocket Chat Message Event
@@ -177,6 +206,35 @@ async function updateViewerCount(viewerCount) {
     await updatePeakViewerCount(viewerCount);
   } catch (error) {
     console.error('Error updating viewer count:', error);
+  }
+}
+
+async function updatePeakViewerCount(viewerCount) {
+  try {
+    const response = await fetch(`http://127.0.0.1:3000/getPeakViewerCount?channel=${encodeURIComponent(channel)}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch peak viewer count');
+    }
+
+    const data = await response.json();
+    const currentPeakViewerCount = data.peakViewerCount;
+
+    if (viewerCount > peakViewerCount) {
+      peakViewerCount = viewerCount;
+      document.getElementById("viewer-peak").textContent =
+        peakViewerCount.toLocaleString();
+    }
+
+    if (viewerCount > currentPeakViewerCount || !currentPeakViewerCount) {
+      // Send request to update peak viewer count in the database
+      await updatePeakViewerCountInDatabase(peakViewerCount);
+      console.log(`Peak viewer count updated to ${peakViewerCount} in the database.`);
+    }
+  } catch (error) {
+    console.error('Error updating peak viewer count:', error);
+    // Display the peak viewer count directly from the current viewer count
+    document.getElementById("viewer-peak").textContent = viewerCount.toLocaleString();
   }
 }
 
@@ -296,10 +354,81 @@ setInterval(fetchViewerCount, 1 * 60 * 1000);
 // update the session duration
 setInterval(updateSessionDuration, 1000);
 
+setInterval(() => {
+  saveUniqueUsernamesToDatabase(channel, uniqueUsernames);
+  saveTopUsernamesToDatabase(channel, topUsernames);
+}, 1 * 30 * 1000);
+
 function updateSessionDuration() {
   const sessionDuration = calculateSessionDuration();
   const sessionDurationElement = document.getElementById("session-duration");
   sessionDurationElement.textContent = sessionDuration;
+}
+
+async function saveUniqueUsernamesToDatabase(channel, uniqueUsernames) {
+  try {
+    // Convert the unique usernames Set to an array and then remove duplicates
+    const uniqueUsernamesArray = Array.from(new Set(uniqueUsernames));
+
+    // Send the filtered unique usernames array to the server for storage
+    await fetch(`http://127.0.0.1:3000/saveUniqueUsernames`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ channel, uniqueUsernames: uniqueUsernamesArray })
+    });
+
+    console.log('Unique usernames saved to the database for channel:', channel);
+  } catch (error) {
+    console.error('Error saving unique usernames to the database:', error);
+  }
+}
+
+async function saveTopUsernamesToDatabase(channel, topUsernames) {
+  try {
+    // Fetch existing top usernames for the channel
+    const response = await fetch(`http://127.0.0.1:3000/getTopUsernames?channel=${encodeURIComponent(channel)}`);
+    const existingData = await response.json();
+
+    // Create a map of existing top usernames for faster lookup
+    const existingUsernamesMap = new Map();
+    existingData.forEach(({ username }) => {
+      existingUsernamesMap.set(username, true);
+    });
+
+    // Insert or update each username-count pair into the database with the specified channel
+    for (const [username, count] of topUsernames) {
+      // Check if the username already exists in the database
+      if (existingUsernamesMap.has(username)) {
+        // Update the count for the existing username
+        await fetch(`http://127.0.0.1:3000/updateTopUsername?channel=${encodeURIComponent(channel)}&username=${encodeURIComponent(username)}&count=${count}`, {
+          method: 'POST'
+        });
+      } else {
+        // Insert the new username-count pair into the database
+        await fetch(`http://127.0.0.1:3000/insertTopUsername?channel=${encodeURIComponent(channel)}&username=${encodeURIComponent(username)}&count=${count}`, {
+          method: 'POST'
+        });
+
+        // Add the inserted username to the existingUsernamesMap to prevent duplicate inserts
+        existingUsernamesMap.set(username, true);
+      }
+    }
+    console.log('Top usernames saved to the database for channel:', channel);
+  } catch (error) {
+    console.error('Error saving top usernames to the database:', error);
+  }
+}
+
+async function updatePeakViewerCountInDatabase(peakViewerCount) {
+  try {
+    await fetch(`http://127.0.0.1:3000/updatePeakViewerCount?channel=${encodeURIComponent(channel)}&peakViewerCount=${peakViewerCount}`, {
+      method: 'POST'
+    });
+  } catch (error) {
+    console.error('Error updating peak viewer count in the database:', error);
+  }
 }
 
 connectWebSocket();
